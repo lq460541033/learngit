e1ea44c30803b59f3ebb8ec3359ef700a9bc05f4
lq <stronger0724@163.com>
2019-04-09 00:28:48 +0800

    maven-ssm-web   lq

---
+++ maven-ssm-web/src/main/java/com/huawei/service/IPersonService.java
@@ -0,0 +1,12 @@
+package com.huawei.service;
+import java.util.List;
+
+import com.huawei.model.Person;
+public interface IPersonService {
+    /**
+     * 加载全部的person
+     * @return
+     */
+    List<Person> loadPersons();
+
+}
